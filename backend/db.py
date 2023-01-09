b95d0ff7fbe85fe10f3d5ec246608ec83cd94f5a
phamlo123 <pham.lo@northeastern.edu>
2023-01-08 21:18:43 -0500

    postgres stuff

--- backend/db.py
+++ backend/db.py
@@ -4,11 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./data.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:pwd@localhost:5433/foundher"
 
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False},echo=True
+    SQLALCHEMY_DATABASE_URL,echo=True
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
---
+++ backend/main.py
@@ -0,0 +1,233 @@
+from datetime import datetime, timedelta
+from typing import Union
+from fastapi.middleware.cors import CORSMiddleware
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+import uvicorn
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from db import get_db, engine
+import sql_app.models as models
+import sql_app.schemas as schemas
+from sql_app.repositories import UserRepo
+from sqlalchemy.orm import Session
+from typing import List,Optional
+from sql_app.schemas import UserCreate , UserBase, User
+
+models.Base.metadata.create_all(bind=engine)
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+current_active_user = None
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = UserRepo.fetch_by_username(db=db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+# async def get_current_user(username: str, db: Session = Depends(get_db)):
+#     credentials_exception = HTTPException(
+#         status_code=status.HTTP_401_UNAUTHORIZED,
+#         detail="Could not validate credentials",
+#         headers={"WWW-Authenticate": "Bearer"},
+#     )
+    
+#     user = UserRepo.fetch_by_username(db=db, username=username)
+#     if user is None:
+#         raise credentials_exception
+#     return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    return current_user
+
+# @app.post("/token", response_model=Token)
+# async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+#     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+#     if not user:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="Incorrect username or password",
+#             headers={"WWW-Authenticate": "Bearer"},
+#         )
+#     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+#     access_token = create_access_token(
+#         data={"sub": user.username}, expires_delta=access_token_expires
+#     )
+#     return {"access_token": access_token, "token_type": "bearer"}
+
+
+
+@app.get("/profile", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    json_compatible_item_data = jsonable_encoder(current_user)
+    return json_compatible_item_data
+
+@app.get("/", response_model=User)
+async def read():
+    return {"welcome": "you"}
+
+
+
+@app.get("/users/me/items/")
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
+
+
+class LoginForm(BaseModel):
+    username: str
+    password: str
+class RegisterForm(LoginForm):
+    email: str
+
+@app.post("/register")
+async def register_user(form_data: RegisterForm, db: Session = Depends(get_db)):
+    username = form_data.username
+    password = form_data.password
+    email = form_data.email
+
+    db_user: User = UserRepo.fetch_by_username(db=db, username=username)
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="username already exists",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    password_hash = get_password_hash(password)
+    new_user = await UserRepo.create(db=db, user=UserCreate(username=username, hashed_password=password_hash, email=email))
+    json_compatible_item_data = jsonable_encoder(new_user)
+    del json_compatible_item_data["hashed_password"]
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": username}, expires_delta=access_token_expires)
+    json_compatible_item_data["access_token"] = access_token
+    return json_compatible_item_data
+
+
+@app.post("/login")
+async def login(form_data: LoginForm, db: Session = Depends(get_db)):
+    username = form_data.username
+    password = form_data.password
+    db_user: User = UserRepo.fetch_by_username(db=db, username=username)
+
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    if not verify_password(password, db_user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    else:
+        json_compatible_item_data = jsonable_encoder(db_user)
+        del json_compatible_item_data["hashed_password"]
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(data={"sub": username}, expires_delta=access_token_expires)
+        json_compatible_item_data["access_token"] = access_token
+        return json_compatible_item_data
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
