03e5fb41320831538d8a2500f14d453a4179c6dd
phamlo123 <pham.lo@northeastern.edu>
2023-01-16 01:46:57 -0500

    finalized

--- backend/db.py
+++ backend/db.py
@@ -4,8 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./data.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://user:pwd@localhost:5433/foundher"
-
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:pwd@localhost:5433/foundher"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:pwd@db/foundher"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,echo=True
--- backend/users/repositories.py
+++ backend/users/repositories.py
@@ -1,7 +1,6 @@
 from sqlalchemy.orm import Session
 
 from . import models, schemas
-
     
 class UserRepo:
     async def create(db: Session, user: schemas.UserCreate):
