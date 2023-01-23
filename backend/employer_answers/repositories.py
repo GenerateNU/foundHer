545951f966cb110c40670437831964c2f68adbf9
Jaime Kvaternik <jaime.kvaternik17@gmail.com>
2023-01-23 18:36:07 -0500

    fix model name

--- backend/employer_answers/repositories.py
+++ backend/employer_answers/repositories.py
@@ -5,20 +5,20 @@ from . import models, schemas
 class EmployerAnswersRepo:
     
  async def create(db: Session, employer_answer: schemas.EmployerAnswerCreate):
-        db_employer_answer = models.Employer_Answer(user_id=employer_answer.user_id,question_id=employer_answer.question_id,answers=employer_answer.answers)
+        db_employer_answer = models.EmployerAnswer(user_id=employer_answer.user_id,question_id=employer_answer.question_id,answers=employer_answer.answers)
         db.add(db_employer_answer)
         db.commit()
         db.refresh(db_employer_answer)
         return db_employer_answer
     
  def fetch_by_id(db: Session,_id):
-     return db.query(models.Employer_Answer).filter(models.Employer_Answer.id == _id).first()
+     return db.query(models.EmployerAnswer).filter(models.EmployerAnswer.id == _id).first()
  
  def fetch_all(db: Session, skip: int = 0, limit: int = 100):
-     return db.query(models.Employer_Answer).offset(skip).limit(limit).all()
+     return db.query(models.EmployerAnswer).offset(skip).limit(limit).all()
  
  async def delete(db: Session,employer_answer_id):
-     db_employer_answer= db.query(models.Item).filter_by(id=employer_answer_id).first()
+     db_employer_answer= db.query(models.EmployerAnswer).filter_by(id=employer_answer_id).first()
      db.delete(db_employer_answer)
      db.commit()
      
