b2de528ef8c357c591614e5db72c0811c0bcad58
isadhwani <isadhwani16@gmail.com>
2023-01-22 21:44:18 -0500

    addded endpoints

--- backend/models.py
+++ backend/models.py
@@ -1,14 +1,13 @@
-import array
 from sqlalchemy import Table, Column, Integer, ForeignKey
-
+from sqlalchemy.types import ARRAY, String
 class Applicant_Question:
     tablename = "applicant_questions"
     id = Column(Integer, primary_key=True,index=True)
-    question_content = Column(str(200), nullable=False, unique=True)
-    possible_answers = Column(array(str(40)))
+    question_content = Column(String(200), nullable=False, unique=True)
+    possible_answers = Column(ARRAY(String(40)))
 
 class Applicant_Answer:
     tablename = "applicant_answers"
     id = Column(Integer, primary_key=True,index=True)
     question_id = Column(Integer, ForeignKey('applicant_questions.id'))
-    answers = Column(array(str(40)))
+    answers = Column(ARRAY(String(40)))
