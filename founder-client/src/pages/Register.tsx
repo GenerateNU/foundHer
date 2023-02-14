ff9113b45334829a87ec03ec7675efbfc1bcfb11
Jaime Kvaternik <jaime.kvaternik17@gmail.com>
2023-02-13 20:20:46 -0500

    add styling

--- founder-client/src/pages/Register.tsx
+++ founder-client/src/pages/Register.tsx
@@ -1,59 +1,58 @@
-import {useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {registerThunk} from "../user/thunks.js";
-import {Navigate} from "react-router";
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router';
 
-const Register = () => {
-    const {currentUser} = useSelector((state: any) => state.users)
-    const [username, setUsername] = useState('alice')
-    const [email, setEmail] = useState('email@gmail.com')
-    const [password, setPassword] = useState('alice1234')
-    
-    // const dispatch = useDispatch<any>()
-    const handleRegisterBtn = () => {
-        // dispatch(registerThunk({username, password, email}))
-    }
-    if(currentUser) {
-        localStorage.setItem("token", currentUser.token)
-        return (<Navigate to={'/profile'}/>)
-    }
+export default function Register() {
+  const { currentUser } = useSelector((state: any) => state.users);
+  const [username, setUsername] = useState('alice');
+  const [email, setEmail] = useState('email@gmail.com');
+  const [password, setPassword] = useState('alice1234');
 
-    return(
-        <>
-            <h1>Register</h1>
-            <div>
-                Username
-                <input
-                onChange={(e) => setUsername(e.target.value)}
-                className="form-control"
-                placeholder="username"
-                value={username}/>
-            </div>
-            <div>
-                Email
-                <input
-                onChange={(e) => setEmail(e.target.value)}
-                className="form-control"
-                placeholder="email"
-                value={email}/>    
-            </div>
-            password      
-            <input
-                onChange={(e) => setPassword(e.target.value)}
-                className="form-control"
-                placeholder="password"
-                type="password"
-                value={password}/>
-            <button
-                className="btn btn-primary w-100"
-                onClick={handleRegisterBtn}>
-                Register
-            </button>
-            {/* {
+  // const dispatch = useDispatch<any>()
+  const handleRegisterBtn = () => {
+    // dispatch(registerThunk({username, password, email}))
+  };
+  if (currentUser) {
+    localStorage.setItem('token', currentUser.token);
+    return <Navigate to={'/profile'} />;
+  }
+
+  return (
+    <>
+      <h1>Register</h1>
+      <div>
+        Username
+        <input
+          onChange={e => setUsername(e.target.value)}
+          className='form-control'
+          placeholder='username'
+          value={username}
+        />
+      </div>
+      <div>
+        Email
+        <input
+          onChange={e => setEmail(e.target.value)}
+          className='form-control'
+          placeholder='email'
+          value={email}
+        />
+      </div>
+      password
+      <input
+        onChange={e => setPassword(e.target.value)}
+        className='form-control'
+        placeholder='password'
+        type='password'
+        value={password}
+      />
+      <button className='btn btn-primary w-100' onClick={handleRegisterBtn}>
+        Register
+      </button>
+      {/* {
                 currentUser &&
                 <h1>Welcome new user: {currentUser.username}</h1>
             } */}
-        </>
-    )
+    </>
+  );
 }
-export default Register
--- founder-client/src/user/reducer.ts
+++ founder-client/src/user/reducer.ts
@@ -1,30 +1,26 @@
-import {createSlice} from "@reduxjs/toolkit";
-import { loginThunk, logoutThunk, registerThunk, profileThunk } from "./thunks";
-
+import { createSlice } from '@reduxjs/toolkit';
 
 const usersReducer = createSlice({
-    name: 'users',
-    initialState: {
-        currentUser: null,
-        loading: false,
-    },
-    // extraReducers: {
-    //     [logoutThunk.fulfilled]: (state, action) => {
-    //         state.currentUser = null
-    //     },
-    //     [registerThunk.fulfilled]: (state, action) => {
-    //         state.currentUser = action.payload
-    //     },
-    //     [loginThunk.fulfilled]: (state, action) => {
-    //         state.currentUser = action.payload
-    //     },
-    //     [profileThunk.fulfilled]: (state, action) => {
-    //         state.currentUser = action.payload
-    //     },
-    // },
-    reducers: {
-
-    }
-})
+  name: 'users',
+  initialState: {
+    currentUser: null,
+    loading: false,
+  },
+  // extraReducers: {
+  //     [logoutThunk.fulfilled]: (state, action) => {
+  //         state.currentUser = null
+  //     },
+  //     [registerThunk.fulfilled]: (state, action) => {
+  //         state.currentUser = action.payload
+  //     },
+  //     [loginThunk.fulfilled]: (state, action) => {
+  //         state.currentUser = action.payload
+  //     },
+  //     [profileThunk.fulfilled]: (state, action) => {
+  //         state.currentUser = action.payload
+  //     },
+  // },
+  reducers: {},
+});
 
 export default usersReducer.reducer;
