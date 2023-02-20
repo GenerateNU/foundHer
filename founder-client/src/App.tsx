5cad6a24443ef80759fc74568340111b7c7bf97a
Jaime Kvaternik <jaime.kvaternik17@gmail.com>
2023-02-19 23:03:06 -0500

    add employer question component/page

--- founder-client/src/App.tsx
+++ founder-client/src/App.tsx
@@ -1,5 +1,4 @@
-import { Routes, Route } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
@@ -8,13 +7,14 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import EmployerQuestionForm from './pages/EmployerQuestionForm/EmployerQuestionForm';
 const store = configureStore({
   reducer: {
     users: usersReducer,
   },
 });
 
-function App() {
+export default function App() {
   return (
     <div className='container mt-4 mb-4'>
       <Provider store={store}>
@@ -24,11 +24,10 @@ function App() {
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
             <Route path='/profile' element={<Profile />} />
+            <Route path='/employer_questions' element={<EmployerQuestionForm />} />
           </Routes>
         </BrowserRouter>
       </Provider>
     </div>
   );
 }
-
-export default App;
