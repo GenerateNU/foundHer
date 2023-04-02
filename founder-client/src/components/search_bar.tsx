f3c4704b098b412ec286a095b2ff4b37f2c190b3
haleymartin-6 <haleymartin@Haleys-MacBook-Air-2.local>
2023-04-02 16:18:57 -0400

    updated search bar with submit button

--- founder-client/src/components/search_bar.tsx
+++ founder-client/src/components/search_bar.tsx
@@ -1,21 +1,34 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 
 function SearchBar(props: { onSearchQueryChange: (arg0: React.SetStateAction<string>) => void; }) {
   const [searchQuery, setSearchQuery] = useState("");
-
+  const dispatch = useDispatch<any>();
+  const handleSubmitBtn = () => {
+    try {
+      dispatch({searchQuery});
+    } catch (e) {}
+  };
+  
   function handleSearchInputChange(event: { target: { value: React.SetStateAction<string>; }; }) {
     setSearchQuery(event.target.value);
     props.onSearchQueryChange(event.target.value);
   }
 
   return (
+    <div>
     <input
       type="text"
       value={searchQuery}
       onChange={handleSearchInputChange}
       placeholder="Search..."
     />
+    <button className='submitButton' onClick={handleSubmitBtn}>
+          Submit
+        </button>
+    </div>
   );
 }
+
 
 export default SearchBar;
---
+++ founder-client/src/pages/Job_Listing_For_Applicants.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import './App.css'
+
+function App () {
+    const [postings, setPostings] = useState([])
+
+useEffect(() => {
+    var postings = []
+    async function fetchData() {
+        const result = await fetch('http://localhost:3001/postings/applicant_id={:id}')
+        const jsonResult = result.json()
+
+        setPostings(await jsonResult)
+    }
+
+    fetchData()
+}, [])
+
+return (
+    <div className='postings_container'>
+        <h2>Postings</h2>
+        {postings.map(posting =>
+            <div key={posting} className='posting_id'>
+                </div>)}
+                </div>
+)
+}
