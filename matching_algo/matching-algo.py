40ad29f273fe8ead84a207bddfd8a44c3d243e1c
Benjamin-Wolff <wolff.benjamind@gmail.com>
2023-04-16 15:28:10 -0400

    Clean up and fix requriements

--- matching_algo/matching-algo.py
+++ matching_algo/matching-algo.py
@@ -1,6 +1,5 @@
 import json
 import requests
-from collections import defaultdict
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
