40e0375d4ebc4def29460bb1b0d8e187e6290254
phamlo123 <pham.lo@northeastern.edu>
2023-04-19 18:57:35 -0400

    navigate to home

--- founder-client/src/components/ProgressBarWrapper/ProgressBarWrapper.tsx
+++ founder-client/src/components/ProgressBarWrapper/ProgressBarWrapper.tsx
@@ -1,17 +1,17 @@
 import React, { Children, useState } from 'react';
 import ProgressBar from './ProgressBar/ProgressBar';
 import './ProgressBarWrapper.css';
-
+import { Navigate, useNavigate } from 'react-router-dom';
 const ProgressBarWrapper = (props: { children: React.ReactNode; markerTitles: string[] }) => {
   const [index, setIndex] = useState(0);
   const elements = Children.toArray(props.children);
   const maxLength = elements.length;
 
   const buttonText = index !== maxLength - 1 ? 'Next' : 'Finish';
-
+  const navigate = useNavigate();
   const handleOnNext = () => {
     if (index === maxLength - 1) {
-      // TODO: send to API
+      navigate("/home");
     } else {
       setIndex(index + 1);
     }
